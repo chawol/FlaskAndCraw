f77e75d1b4a03778c24a4952432b8ff1c05e0294
chawol <joe9360110000@gmail.com>
2021-10-28 22:40:53 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests as rq
+import json
+
+from flask import Flask
+
+app = Flask(__name__)
+url = "http://www.leonland.de/elements_by_price/en/list"
+response = rq.get(url).text
+soap = BeautifulSoup(response, 'html.parser')
+table = soap.find(id='tlst')
+json_data = {}
+datas = []
+for i in table.find_all('tr')[1:]:
+    ele = i.find_all('td')
+    data = {"id": ele[0].text, "Price": ele[3].text}
+    datas.append(data)
+
+    # j = {"id": ele[0].text, "value": ele[3].text}
+    # print(ele[0].text + ":" + ele[3].text)
+json_data = json.dumps(datas)
+
+
+@app.route("/")
+def json():
+    return json_data
+
+
+if __name__ == "__main__":
+    app.run()
